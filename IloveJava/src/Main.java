be09ee6beda12e6b678bdef5d572a982da0646f9
Orcun <Orcun@10.48.155.189>
2022-10-31 09:49:59 +0300

    ders5

--- IloveJava/src/Main.java
+++ IloveJava/src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 import java.io.IOException;
 import java.util.*;
 
@@ -7,8 +6,36 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("hello");
+		ArrayList<String> item = new ArrayList<String>();
+		
+		item.add("red");
+		item.add("yellow");
+		item.add(0,"green");
+		
+		for(String color : item) {
+			System.out.print(color + " ");
+		}
+		item.remove("red");
+		System.out.println();
+		for(String color : item) {
+			System.out.print(color + " ");
+		}
+		System.out.println();
+		System.out.println(item.get(1));
+		System.out.println(item.contains("yellow"));
+		
+		
+		
+		
+		
+		
 		
 	}
+	
+	
+		
+			
+	
+	
 
 }
